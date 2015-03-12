808f71f792ce6d4763fa218cd5a6c3d1782d382d
Kamil Jarmużek <kamil.jarmuzek@gmail.com>
2015-03-12 21:46:10 +0100

    [WS] Initial project + test methods

---
+++ Ws/ILaggerService.cs
@@ -0,0 +1,46 @@
+﻿using LaggerServer.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LaggerServer
+{
+    [ServiceContract]
+    public interface ILaggerService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "test/connection", ResponseFormat = WebMessageFormat.Json)]
+        string TestConnection();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "test/connection/{value}", ResponseFormat = WebMessageFormat.Json)]
+        string TestConnectionValue(string value);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "test/json", ResponseFormat = WebMessageFormat.Json)]
+        TestowaKlasa TestJsonGet();
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "test/json", Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        TestowaKlasa TestJsonPost(TestowaKlasa value);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "Test/{id}")]
+        User GetUser(string id);
+    }
+
+
+    [DataContract]
+    public class TestowaKlasa
+    {
+        [DataMember]
+        public String Login { get; set; }
+
+        [DataMember]
+        public String Password { get; set; }
+    }
+}
---
+++ Ws/LaggerService.svc.cs
@@ -0,0 +1,50 @@
+﻿using LaggerServer.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LaggerServer
+{
+    public class LaggerService : ILaggerService
+    {
+        public User GetUser(string id)
+        {
+            using (var ctx = new LaggerDbEntities())
+            {
+                var user = ctx.Users.Where(x => x.ID_User == Convert.ToInt32(id)).FirstOrDefault();
+
+                return user;
+            }
+        }
+
+        public string TestConnection()
+        {
+            return "Connected success.";
+        }
+
+        public string TestConnectionValue(string value)
+        {
+            return "You sended value: " + value;
+        }
+
+        public TestowaKlasa TestJsonGet()
+        {
+            return new TestowaKlasa()
+            {
+                Login = "Test",
+                Password = "Tajne"
+            };
+        }
+
+        public TestowaKlasa TestJsonPost(TestowaKlasa value)
+        {
+            value.Login += " Nowy";
+            value.Password += " Nowy";
+            return value;
+        }
+    }
+}
