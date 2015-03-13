96c0d185767791651c8276fc8e1e162da0a82de4
Jakub Myka <j.myka@tmssoft.pl>
2015-03-13 19:52:59 +0100

    structure

--- app/app/src/main/java/com/android/lagger/forms/login/LoginActivity.java
+++ app/app/src/main/java/com/android/lagger/forms/login/LoginActivity.java
@@ -1,24 +1,25 @@
-package com.android.lagger;
+package com.android.lagger.forms.login;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.lagger.R;
 
-public class MainActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
 
